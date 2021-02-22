9c2a0c4f1235f322da6878cc76e370d25a6d9df1
Alexandre Riquet <alexandre.riquet@gmail.com>
2021-02-22 15:04:04 +0100

    Generate gradient direction

--- src/index.js
+++ src/index.js
@@ -1,41 +1,76 @@
 const state = {
   colors: ['#e367a4', '#48b1f3'],
-  direction: '90deg',
+  directions:[0,45,90,135,180,225,270,315],
+  currentDirection: 90,
 };
 
 const init = () => {
-  makeColors();
+  makeBtnsDirection();
+  makeLegendColors();
   makeGradient();
+  makeCssCode();
+};
+
+const randomHexaColorGenerator = () => `#${Math.random().toString(16).slice(2, 8)}`;
+
+const makeBtnsDirection = () => {
+  const {directions, currentDirection} = state;
+  const container = document.getElementById('directionBtns');
+  container.innerHTML = '';
+
+  directions.forEach(direction => {
+    console.log(direction, currentDirection);
+    const btn = document.createElement('button');
+    btn.className = 'button';
+    btn.setAttribute('data-direction', direction);
+    btn.textContent = direction;
+    if(direction === currentDirection) btn.classList.add('current');
+    btn.addEventListener('click', (e) => handleChangeDirection(e.target.dataset.direction));
+    container.append(btn);
+  });
 }
 
-// Générer 2 couleurs aléatoires
-const randomHexaColorGenerator = () => `#${Math.random().toString(16).slice(2, 8)}`;
-// Générer les colors
-const makeColors = () => {
-  const {colors} = state;
+const makeLegendColors = () => {
+  const {colors, currentDirection} = state;
   const colorsContainer = document.getElementById('colors');
+
+  colorsContainer.innerHTML= '';
 
   colors.forEach((color, index) => {
     const span = document.createElement('span');
     span.style.color = color;
     span.textContent = color;
+
+    const spanArrow = document.createElement('span');
+    spanArrow.id = 'directionArrow';
+    spanArrow.style.transform = `rotate(${currentDirection}deg)`;
+    spanArrow.textContent = ' ↑ ';
+
     colorsContainer.append(span);
-    
-    if(index < colors.length-1) colorsContainer.append(' → ');
+
+    if(index < colors.length-1) colorsContainer.append(spanArrow);
 
   });
 
 };
 
-// Appliquer les colors au dégradé
 const makeGradient = () => {
-  const {colors} = state;
+  const {colors, currentDirection} = state;
   const gradient = document.getElementById('gradient');
-  gradient.style.background = `linear-gradient(90deg, ${[...colors]})`;
-}
+  gradient.style.background = `linear-gradient(${currentDirection}deg, ${[...colors]})`;
+};
 
-// Générer un dégradé
-// Changer le direction du dégradé
-// Renseigner le code CSS
+const makeCssCode = () => {
+  const {colors, currentDirection} = state;
+  const cssCodeGenerator = document.getElementById('cssCodeGenerator');
+  const code = `background-color: linear-gradient(${currentDirection}deg, ${[...colors]});`;
+  cssCodeGenerator.innerHTML = '';
+  cssCodeGenerator.append(code);
+};
+
+const handleChangeDirection = (newDirection) => {
+  state.currentDirection = Number(newDirection);
+  init();
+};
 
 document.addEventListener("DOMContentLoaded", init());
